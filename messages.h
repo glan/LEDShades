42bd6ea07f53b7e23ad8a4f1be7ad08682191012
Glan Thomas <glanthomas@mac.com>
2013-05-20 09:31:34 -0700

    Imported inital MaceTech code

---
+++ messages.h
@@ -0,0 +1,12 @@
+// Scrolling messages
+
+char string0[] PROGMEM = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679   ";
+char string1[] PROGMEM = "macetech.com   ";
+char string2[] PROGMEM = "Maker Faire May 18 & 19  ";
+
+const char * stringArray[] PROGMEM = {
+  string0,
+  string1,
+  string2
+};
+
